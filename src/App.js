610e14ed10e80c36aa19eb51ed7f13f9ff8eeb0f
Achrefgasri2 <gasriachref@gmail.com>
2021-11-16 22:25:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div>
         <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
           <h1 className="title">Achref Gasri </h1>
-          <img src={pc} width={320} height={240}/>
-          <img src="/img/iphone.png" width={320} height={240} />
+          <img src={pc} width={320}  alt="PC"/>
+          <img src="/img/iphone.png" width={320} alt="IPHONE" />
         </div>
         <video width={320} height={240} controls>
         <source src={html} type="video/mp4" />
